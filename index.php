6fbf0108456be475e8799c2a7926b211803b3ed5
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-07-19 03:25:59 -0500

    Added js to things

--- index.php
+++ index.php
@@ -5,8 +5,22 @@
         <meta name="viewport" content="width=device-width,
                     initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible">
-        <title> Change Me </title>
+        <link rel="stylesheet" href="css/styles.css">
+        <title> Pay Page </title>
     </head>
     <body>
+        <form action="/charge">
+            <div class="form-row">
+                <label for="card-element">
+                    Credit or debit card
+                </label>
+                <div id="card-element">
+                    <!-- A Stripe Element will be inserted here. -->
+                </div>
+
+                <!-- Used to display form errors -->
+                <div id="card-errors" role="alert"></div>
+            </div>
+        </form>
     </body>
 </html>
