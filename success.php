cb9bb4160ada5f5fc2dfb97c3e7cd14526954024
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-07-19 15:55:30 -0500

    Corrected syntax error

--- success.php
+++ success.php
@@ -1,6 +1,11 @@
 <?php
-if (!empty($_GET['tid']) && !empty['product']) {
+if (!empty($_GET['tid']) && !empty($_GET['product'])) {
     $GET = filter_var_array($_GET, FILTER_SANITIZE_STRING);
+
+    $tid = $GET['tid'];
+    $product = $GET['product'];
+} else {
+    header('Location: index.php');
 }
 ?>
 
