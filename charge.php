d0a4d85f27eba13278999db9429018f1f97fa599
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-07-19 14:53:16 -0500

    Basic string sanitizing from fields

--- charge.php
+++ charge.php
@@ -1,2 +1,12 @@
 <?php
 require_once('vendor/autoload.php');
+
+\Stripe\Stripe::setApiKey('sk_test_UqbYcqyb1MhZbYxq8XvuXn7h00imeKMLGE');
+
+// Sanitize POST
+$POST = filter_var_array($_POST, FILTER_SANITIZE_STRING);
+
+$first_name = $POST['first_name'];
+$last_name = $POST['last_name'];
+$email = $POST['email'];
+$token = $POST['stripeToken'];
