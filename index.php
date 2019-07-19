597796ebba79f384115608c55385d9955ec437c8
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-07-19 04:37:12 -0500

    Add material design bootstrap

--- index.php
+++ index.php
@@ -6,6 +6,12 @@
                     initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible">
         <link rel="stylesheet" href="css/styles.css">
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+        <!-- Bootstrap core CSS -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Material Design Bootstrap -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.5/css/mdb.min.css" rel="stylesheet">
         <title> Pay Page </title>
     </head>
     <body>
