f4573a51298e8f82d456078ffdf18552dc810a19
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-07-19 15:33:19 -0500

    Added customer and charge

--- charge.php
+++ charge.php
@@ -12,4 +12,14 @@ $email = $POST['email'];
 $token = $POST['stripeToken'];
 
 // Crear un cliente en Stripe
-$customer = \Stripe\Customer::create(array());
+$customer = \Stripe\Customer::create(array(
+    "email" => $email,
+    "source" => $token
+));
+
+$charge = \Stripe\Charge::create(array(
+    "amount" => 5000,
+    "currency" => MXN,
+    "description" => "My Arms Your Hearse",
+    "customer" => $customer -> id
+));
