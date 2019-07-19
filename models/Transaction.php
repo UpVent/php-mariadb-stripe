965960b9a5e25174b340c1cdf34745fe1871eadc
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-07-19 18:19:45 -0500

    Add getTransactions function to model

--- models/Transaction.php
+++ models/Transaction.php
@@ -26,4 +26,12 @@ class Transaction {
            return false;
        }
     }
+
+    public function getTransactions() {
+       $this->db->query('SELECT * FROM transactions ORDER BY created_at DESC');
+
+       $results = $this->db->resultset();
+
+       return $results;
+    }
 }
