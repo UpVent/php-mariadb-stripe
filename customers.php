abc9504de10b203d8d91c3ed10484a6224fceb12
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-07-19 17:54:24 -0500

    Base para el archivo de clientes

--- customers.php
+++ customers.php
@@ -1,1 +1,43 @@
 <?php
+require_once('config/db.php');
+require_once('lib/pdo_db.php');
+require_once('models/customer.php');
+
+
+// Instanciar un cliente
+$customer = new Customer();
+
+// Obtener un cliente
+$customers = $customer->getCustomers();
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible">
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+        <!-- Bootstrap core CSS -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Material Design Bootstrap -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.5/css/mdb.min.css" rel="stylesheet">
+        <title> Ver Clientes </title>
+    </head>
+    <body>
+        <div class="container mt-4">
+            <h2>Clientes</h2>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>ID del cliente</th>
+                        <th>Nombre</th>
+                        <th>Email</th>
+                        <th>Fecha</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    </body>
+</html>
